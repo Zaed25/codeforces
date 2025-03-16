9af5dad4fd6c61450139b06f04e9f3d91f6d8de7
zaidNa03 <alnahhaszaed27@gmail.com>
2025-03-16 12:30:28 +0300

    Initial Commit

---
+++ codeforces/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  codeforces
+//
+//  Created by Mhd Zaid Al Nahhas on 16/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
